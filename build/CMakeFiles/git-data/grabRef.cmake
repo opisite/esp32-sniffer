9773cf56a70d3b3c1c379eafd6c90a393f80b8c9
aabedini.c <aabedini.c@arlo.com>
2024-02-19 19:24:12 -0800

    Actually initialized submodule for UART manager

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/home/vboxuser/Repos/esp32-sniffer/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/vboxuser/esp/esp-idf/.git")
+set(GIT_DIR "/home/vboxuser/Repos/esp32-sniffer/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
--- main/main.c
+++ main/main.c
@@ -8,22 +8,6 @@
 #include "wifi_config.h"
 #include "driver/uart.h"
 
-const uart_config_t uart_config = {
-    .baud_rate = 115200,
-    .data_bits = UART_DATA_8_BITS,
-    .parity = UART_PARITY_DISABLE,
-    .stop_bits = UART_STOP_BITS_1,
-    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-    .source_clk = UART_SCLK_APB,
-};
-
-static void initialize_uart() {
-    int uart_num = UART_NUM_1;
-    uart_driver_install(uart_num, 2048, 0, 0, NULL, 0);
-    uart_param_config(uart_num, &uart_config);
-    uart_set_pin(uart_num, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-
-}
 
 void app_main(void) {
     // Initialize NVS
